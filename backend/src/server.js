5bbafa457e0691421ae8776304b548b9e881f9ed
kenneac <iamkenneac@gmail.com>
2026-08-14 11:21:56 +0100

    initial commit for backend and mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,43 @@
+// IMPORTS
 import express from "express";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
-
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
+
+// CONSTANTS
 const app = express();
 
+
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-
 app.use(clerkMiddleware());
 app.use(arcjetMiddleware);
 
-app.get("/", (req, res) => res.send("Hello from server"));
 
+// ROUTES
+app.get("/", (req, res) => res.send("Hello from server"));
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-// error handling middleware
+
+// ERROR HANDLING MIDDLEWARE
 app.use((err, req, res, next) => {
   console.error("Unhandled error:", err);
   res.status(500).json({ error: err.message || "Internal server error" });
 });
 
+
+//START SERVER
 const startServer = async () => {
   try {
     await connectDB();
@@ -45,8 +51,8 @@ const startServer = async () => {
     process.exit(1);
   }
 };
-
 startServer();
 
-// export for vercel
+
+// EXPORT FOR VERCEL
 export default app;

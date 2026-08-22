19e1580d11b62b7b3744d172f21a585027cb082b
kenneac <iamkenneac@gmail.com>
2026-08-22 14:16:12 +0100

    started the frontend

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,17 +1,10 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View className=" flex-1 justify-center items-center bg-white">
+      <Text className="text-red-500">Edit src/app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
